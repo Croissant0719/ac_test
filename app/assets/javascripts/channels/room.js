df4547d3f6487243c68fd735f806b5dc9013a81a
Koji Igarashi <koji.igarashi.0719@gmail.com>
2018-12-26 14:59:19 +0900

    Add input message alerting system

---
+++ app/assets/javascripts/channels/room.js
@@ -0,0 +1,23 @@
+App.chat = App.cable.subscriptions.create("RoomChannel", {
+
+  connected: function() {
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received: function(data) {
+    // Called when there's incoming data on the websocket for this channel
+  }
+},
+
+$(document).on('keypress', '[data-behavior~=room_speaker]', function(event) {
+    if (event.keyCode === 13) {
+      App.room.speak(event.target.value);
+      event.target.value = '';
+      return event.preventDefault();
+    }
+  })
+);
